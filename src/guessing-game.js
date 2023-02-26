28ea5ed2d77bde41f6c20a64d7d8e6d42d1e3ea1
Nastya <zhitnikovich3308@gmail.com>
2023-02-26 19:19:51 +0300

    init: start task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minNum = min;
+        this.maxNum = max;
     }
 
     guess() {
-
+        let aver = Math.round( (this.minNum + this.maxNum) / 2 );
+        this.result = aver;
+        return this.result;
     }
 
     lower() {
-
+        this.maxNum = this.guess();
     }
 
     greater() {
-
+        this.minNum = this.guess();
     }
 }
 
